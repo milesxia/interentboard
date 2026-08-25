81b70342f51c3cfaaf579275a7eaf77699e2f9e4
milesxia <162951536+milesxia@users.noreply.github.com>
2026-08-25 07:07:19 +0000

    Initial IntelBoard v0.2.0

--- intelboard-release/scripts/codespaces-push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-: "${DOCKERHUB_USERNAME:?Set DOCKERHUB_USERNAME}"
-: "${DOCKERHUB_TOKEN:?Set DOCKERHUB_TOKEN}"
-echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
-docker build -t "$DOCKERHUB_USERNAME/intelboard:latest" .
-docker push "$DOCKERHUB_USERNAME/intelboard:latest"
-echo "Pushed: $DOCKERHUB_USERNAME/intelboard:latest"
